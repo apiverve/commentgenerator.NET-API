1d1d13bfc85852933c658ae984751bb917c55ae4
APIVerve Team <hello@apiverve.com>
2025-02-21 00:48:59 -0600

    Added new API Endpoints

---
+++ CommentGeneratorQueryOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CommentGeneratorQueryOptions {
+public string mode { get; set; }
+public string tone { get; set; }
+public integer count { get; set; }
+public boolean emojis { get; set; }
+}
+
+}
